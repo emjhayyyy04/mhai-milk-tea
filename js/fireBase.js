bc7c166b5a76ccf7a163105b8c5ff8e37b5735cb
emjhayyyy04 <100015657+emjhayyyy04@users.noreply.github.com>
2022-02-19 15:11:11 +0800

    Add files via upload

---
+++ js/fireBase.js
@@ -0,0 +1,16 @@
+var app_fireBase = {};
+(function(){
+  var firebaseConfig = {
+    apiKey: "AIzaSyCcvqBviS4UgglaiEGo2sFQL6NkijpFJ_c",
+    authDomain: "loginform-8ea24.firebaseapp.com",
+    projectId: "loginform-8ea24",
+    storageBucket: "loginform-8ea24.appspot.com",
+    messagingSenderId: "101388802304",
+    appId: "1:101388802304:web:2f06113af25355c645bbe5"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  app_fireBase = firebase;
+})()
+
